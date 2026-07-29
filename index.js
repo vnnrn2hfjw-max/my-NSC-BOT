6a885f979094aadad2f5cb1aecaf01d760f782c1
vnnrn2hfjw-max <vnnrn2hfjw@privaterelay.appleid.com>
2026-07-29 14:32:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder
+} = require("discord.js");
+
+const { REST, Routes } = require("discord.js");
+
 
 const client = new Client({
   intents: [
@@ -6,30 +15,102 @@ const client = new Client({
   ]
 });
 
-client.once(Events.ClientReady, () => {
-  console.log(`✅ Logged in as ${client.user.tag}`);
-});
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+// Slash commands
+const commands = [
+  {
+    name: "ping",
+    description: "Check bot performance"
+  }
+];
 
+
+const rest = new REST({ version: "10" })
+.setToken(process.env.TOKEN);
+
+
+const CLIENT_ID = "1531640039820230847";
+
+
+// Register commands
+(async () => {
   try {
-    if (interaction.commandName === "ping") {
-      await interaction.reply("🏓 Pong!");
-    }
+
+    console.log("Registering commands...");
+
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      {
+        body: commands
+      }
+    );
+
+    console.log("✅ Commands registered!");
+
   } catch (error) {
+
     console.error(error);
 
-    if (!interaction.replied) {
-      await interaction.reply({
-        content: "❌ Something went wrong.",
-        ephemeral: true
-      });
-    }
   }
+})();
+
+
+// When bot starts
+client.once("ready", () => {
+
+  console.log(`✅ ${client.user.tag} is online!`);
+
 });
 
+
+// Commands
+client.on("interactionCreate", async interaction => {
+
+  if (!interaction.isChatInputCommand()) return;
+
+
+  if (interaction.commandName === "ping") {
+
+
+    const start = Date.now();
+
+    await interaction.deferReply();
+
+
+    const latency = Date.now() - start;
+
+
+    const embed = new EmbedBuilder()
+
+    .setColor("#8B0000")
+
+    .setTitle("🔴⚫ NSC Bot Status")
+
+    .setDescription(
+`🟢 **Status:** Online
+
+🏓 **Bot Latency**
+\`${latency}ms\`
+
+🌐 **API Latency**
+\`${client.ws.ping}ms\`
+
+━━━━━━━━━━━━━━
+
+⚫ NSC • No Second Chances`
+    )
+
+    .setTimestamp();
+
+
+    await interaction.editReply({
+      embeds: [embed]
+    });
+
+  }
+
+});
+
+
+// Login
 client.login(process.env.TOKEN);
-if (interaction.commandName === "help") {
-  await interaction.reply("Commands: /ping /help");
-}
